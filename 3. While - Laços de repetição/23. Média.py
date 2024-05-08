af4b4e3a0d7f81a184c34ceb0dbb61ca1e60ab66
IgorMiranda56 <159519511+IgorMiranda56@users.noreply.github.com>
2024-05-08 12:28:39 +0000

    Exercícios de Python

--- 3. While - Laços de repetição/23. Média.py
+++ 3. While - Laços de repetição/23. Média.py
@@ -2,12 +2,12 @@ import os
 
 os.system("cls || clear")
 
-QUANTIDADE_NOTAS = 2
+QUANTIDADE_NOTAS = 3
 soma = 0
 
 for i in range(QUANTIDADE_NOTAS):
     while True: 
-        nota = float(input(f"Digite sua {i+1}ª nota (entre 0 e 10): "))
+        nota = float(input(f"Digite sua {i+1}ª nota: "))
 
         if nota < 0 or nota > 10:
             print("Nota inválida. Tente novamente. \n")
@@ -18,3 +18,9 @@ for i in range(QUANTIDADE_NOTAS):
 media = soma / QUANTIDADE_NOTAS
 
 print(f"Média: {media}")
+if media >= 7:
+    print("Aprovado.")
+elif media >= 5:
+    print("Recuperação.")
+else:
+    print("Reprovado.")
